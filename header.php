c7b361cf03c7020b4029d75f883ac34a02d5eed6
Sudheer <21117963+sd4483@users.noreply.github.com>
2024-06-22 20:40:00 +0530

    Include css file in header.php

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CS-Simplified</title>
+    <style>
+        <?php include 'CSS/style.css'; ?>
+    </style>
 </head>
 <body>
     <header>
